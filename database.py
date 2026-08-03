0eaf5141b87ea5ab6f70557769ba4d3d82de6ad8
vovas <vova.shigaev121183@gmail.com>
2026-08-03 23:23:28 +0200

    6.2

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import json
-
+import sqlite3
 
 def load_users():
 
@@ -19,3 +19,42 @@ def save_users(users):
             ensure_ascii=False,
             indent=4
         )
+
+        # ==================================================
+        # Получить пользователей из SQLite
+        # ==================================================
+
+        def load_users_sqlite():
+            # Подключаемся к базе данных
+            connection = sqlite3.connect("database.db")
+
+            # Создаём cursor
+            cursor = connection.cursor()
+
+            # Выполняем SQL-запрос
+            cursor.execute("""
+            SELECT name, job, city
+            FROM users
+            """)
+
+            # Получаем все записи
+            rows = cursor.fetchall()
+
+            # Создаём пустой список
+            users = []
+
+            # Преобразуем кортежи в словари
+            for row in rows:
+                user = {
+                    "name": row[0],
+                    "job": row[1],
+                    "city": row[2]
+                }
+
+                users.append(user)
+
+            # Закрываем соединение
+            connection.close()
+
+            # Возвращаем список
+            return users
